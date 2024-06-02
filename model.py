9d388f796a7ddd720d483591e3b98340def8c6bb
Antariksh Sisodiya <45943690+akshsisodiya@users.noreply.github.com>
2024-06-02 06:17:31 +0000

    decoder done

--- model.py
+++ model.py
@@ -123,3 +123,38 @@ class EncoderBlock(nn.Module):
         x = self.residual_connection[0](x, lambda x: self.self_attention_block(x,x,x,src_mask))
         x = self.residual_connection[1](x, self.feed_forward_block)
         return 
+    
+class Encoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
+    
+class DecoderBlock(nn.Module):
+    def __init__(self, self_attention_block:MultiHeadAttentionBlock, cross_attention_block:MultiHeadAttentionBlock, feed_forward_blockf:FeedForwardBlock, dropout:float,*args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_blockf
+        self.residual_connection = nn.ModuleList([ResidualConnection(dropout) for _ in range(3)])
+
+    def forward(self, x, encoder_output, src_mask, target_mask):
+        x = self.residual_connection[0](x, lambda x: self.self_attention_block(x,x,x,target_mask))
+        x = self.residual_connection[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
+        x = self.residual_connection[2](x, self.feed_forward_block)
+        return x
+    
+class Decoder(nn.Module):
+    def __init__(self, layers:nn.ModuleList, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.layers = layers
+        self.norm = LayerNormalization()
+    
+    def forward(self, x, encoder_output, src_mask, target_mask):
+        for layer in self.layers:
+            x = layer(x, encoder_output, src_mask, target_mask)
